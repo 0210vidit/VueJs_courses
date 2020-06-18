7dc05d8167c7f5202ed11236ecd346310cdd8010
Vidit Shah <0210vidit@gmail.com>
2020-06-19 04:05:35 +0530

    vue prop

--- Max/Section5VueInstance/app.js
+++ Max/Section5VueInstance/app.js
@@ -1,26 +1,28 @@
 new Vue({
-  el: '#app',
+  el: '#app1',
   data: {
     title: 'The VueJS Instance',
-    showParagraph: false
+    showParagraph: false,
   },
   methods: {
-    show: function() {
+    show: function () {
       this.showParagraph = true;
       this.updateTitle('The VueJS Instance (Updated)');
     },
-    updateTitle: function(title) {
+    updateTitle: function (title) {
       this.title = title;
-    }
+    },
   },
   computed: {
-    lowercaseTitle: function() {
+    lowercaseTitle: function () {
       return this.title.toLowerCase();
-    }
+    },
   },
   watch: {
-    title: function(value) {
+    title: function (value) {
       alert('Title changed, new value: ' + value);
-    }
-  }
+    },
+  },
 });
+
+new Vue({});
