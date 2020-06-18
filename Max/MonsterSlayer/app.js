77b2f3d3e7915508b4d77929c0d059c16cf403c7
Vidit Shah <0210vidit@gmail.com>
2020-06-18 23:37:38 +0530

    buttons

--- Max/MonsterSlayer/app.js
+++ Max/MonsterSlayer/app.js
@@ -11,9 +11,54 @@ new Vue({
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
-    attack: function () {},
-    specialAttack: function () {},
-    heal: function () {},
-    giveUp: function () {},
+    attack: function () {
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttack();
+    },
+    specialAttack: function () {
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttack();
+    },
+    heal: function () {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      }
+      this.monsterAttack();
+    },
+    giveUp: function () {
+      this.gameIsRunning = false;
+    },
+    calculateDamage: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function () {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You Won! New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm('You Lost! New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
+    monsterAttack: function () {
+      this.playerHealth -= this.calculateDamage(5, 12);
+
+      this.checkWin();
+    },
   },
 });
