09302ecfa6833d6e29f304fd59f9fd104702c74e
Vidit Shah <0210vidit@gmail.com>
2020-06-21 03:36:57 +0530

    b7

--- TheNetNinja/phase1/1_Basics/app.js
+++ TheNetNinja/phase1/1_Basics/app.js
@@ -6,6 +6,10 @@ new Vue({
     url: 'http://google.com',
     classes: ['one', 'two'],
     wage: 10,
+    coords: {
+      x: 0,
+      y: 0,
+    },
   },
   methods: {
     greet(time) {
@@ -13,6 +17,13 @@ new Vue({
     },
     changeWage(amount) {
       this.wage += amount;
+    },
+    logEvent(e) {
+      console.log(e);
+    },
+    logCoords(e) {
+      this.coords.x = e.offsetX;
+      this.coords.y = e.offsetY;
     },
   },
 });
